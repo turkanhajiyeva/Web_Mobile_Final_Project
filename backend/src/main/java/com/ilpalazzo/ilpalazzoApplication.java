65a02f0f29724504ed99f1ccb7e68f2e0da33378
turkanhajiyeva <thajiyeva15574@ada.edu.az>
2025-05-11 23:08:01 +0400

    Gradle, Spring boot added

---
+++ backend/src/main/java/com/ilpalazzo/ilpalazzoApplication.java
@@ -0,0 +1,11 @@
+package com.ilpalazzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ilpalazzoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ilpalazzoApplication.class, args);
+    }
+}
