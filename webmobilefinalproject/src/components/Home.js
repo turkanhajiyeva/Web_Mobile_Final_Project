74d879de7bdcfa6e2c46cdd8e83de21504cc03f6
Surkhay Fatullayev <surkhayfatullayev@novum.tech>
2025-05-11 15:04:50 +0400

    fit horse

--- webmobilefinalproject/src/components/Home.js
+++ webmobilefinalproject/src/components/Home.js
@@ -10,20 +10,15 @@ const Home = () => {
         <h1>IL'PALAZZO PIZZA</h1>
 
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img class="d-block w-100" src="./images/horsin_around.jpg" alt="First slide"/>
+            <img class="carousel-custom-height d-block w-100" src="./images/horsin_around.jpg" alt="First slide"/>
             </div>
             <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Second slide"/>
+            <img class="carousel-custom-height d-block w-100" src="..." alt="Second slide"/>
             </div>
             <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Third slide"/>
+            <img class="carousel-custom-height d-block w-100" src="..." alt="Third slide"/>
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
