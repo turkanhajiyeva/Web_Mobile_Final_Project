00501cae5e4b75bae89797c93f760496a43f7c93
Surkhay Fatullayev <surkhayfatullayev@novum.tech>
2025-05-14 05:04:48 +0400

    added ability to change order status(why orderid is not guid anw)

--- backend/src/main/java/com/ilpalazzo/service/OrderService.java
+++ backend/src/main/java/com/ilpalazzo/service/OrderService.java
@@ -1,12 +1,13 @@
 package com.ilpalazzo.service;
 
-import com.ilpalazzo.model.entity.Order;
-
 import java.util.List;
+
+import com.ilpalazzo.model.entity.Order;
 
 public interface OrderService {
     Order placeOrder(Order order);
     List<Order> getAllOrders();
     Order getOrderById(Long id);
     void deleteOrder(Long id);
+    Order updateOrderStatus(Long orderId, String newStatus);
 }
