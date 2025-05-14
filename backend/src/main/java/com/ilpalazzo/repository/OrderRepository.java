dc3258a8dcf783d0981f3a77a5b7147009b8bc07
turkanhajiyeva <thajiyeva15574@ada.edu.az>
2025-05-14 22:52:56 +0400

    Waiter notif done

--- backend/src/main/java/com/ilpalazzo/repository/OrderRepository.java
+++ backend/src/main/java/com/ilpalazzo/repository/OrderRepository.java
@@ -2,6 +2,10 @@ package com.ilpalazzo.repository;
 
 import com.ilpalazzo.model.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByStatus(String status);
 }
