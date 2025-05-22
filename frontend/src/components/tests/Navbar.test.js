8c5e123939b9e02cb5eb24d540c8c9a931eb382e
RamizT2003 <91952906+RamizT2003@users.noreply.github.com>
2025-05-22 19:44:15 +0400

    Added unit testing of MenuItemCard.test.js and rest but rest are like demo for now

--- frontend/src/components/tests/Navbar.test.js
+++ frontend/src/components/tests/Navbar.test.js
@@ -1,17 +1,17 @@
-// import { render, screen } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom';
-// //import Navbar from '../Navbar';
-// import Navbar from '../Navbar';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+//import Navbar from '../Navbar';
+import Navbar from '../Navbar';
 
-// describe('Navbar', () => {
-//   test('renders logo and links', () => {
-//     render(
-//       <BrowserRouter>
-//         <Navbar />
-//       </BrowserRouter>
-//     );
+describe('Navbar', () => {
+  test('renders logo and links', () => {
+    render(
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    );
 
-//     expect(screen.getByText(/il palazzo/i)).toBeInTheDocument();
-//     expect(screen.getByRole('link', { name: /menu/i })).toBeInTheDocument();
-//   });
-// });
+    expect(screen.getByText(/il palazzo/i)).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: /menu/i })).toBeInTheDocument();
+  });
+});
